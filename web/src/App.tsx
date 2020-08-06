c168135eede96ff70a11ef428387f15b256293ae
Edyr Costa <edyrcosta12@gmail.com>
2020-08-05 22:02:59 -0300

    Alteração para retornar as rotas do app.

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 
+import Routes from "./routes";
+
 import "./assets/styles/global.css";
 
 function App() {
-  return <></>;
+  return <Routes />;
 }
 
 export default App;
